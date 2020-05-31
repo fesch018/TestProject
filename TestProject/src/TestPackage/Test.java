9769880c0d710b7c2c83e1c1049e604258b38916
fesch018 <64429576+fesch018@users.noreply.github.com>
2020-05-31 21:51:46 +0200

    hab eine for schleife und ein paar Variablen hinzugefügt

--- TestProject/src/TestPackage/Test.java
+++ TestProject/src/TestPackage/Test.java
@@ -4,7 +4,14 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
-
+		int i = 0;
+		int j = 1;
+		
+		for(int x = 0; x<10; x++) {
+			i++;
+			j= i+j;
+		}
+		System.out.println(j);
 	}
 
 }
