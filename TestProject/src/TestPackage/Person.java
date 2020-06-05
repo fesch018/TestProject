08043ec29182828847a924f2b2f8063a39af771c
fesch018 <64429576+fesch018@users.noreply.github.com>
2020-06-05 21:32:53 +0200

    Habe Klasse perso hinzugefügt

---
+++ TestProject/src/TestPackage/Person.java
@@ -0,0 +1,16 @@
+package TestPackage;
+
+public class Person {
+	String vorname;
+	String nachname;
+	public Person(String vorname, String nachname) {
+		this.vorname = vorname;
+		this.nachname = nachname;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+}
